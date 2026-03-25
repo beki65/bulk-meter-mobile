1f71f4628d026e491108e664d7cb16b9c6de2dbb
beki65 <bekig65@gmail.com>
2026-03-25 08:37:13 +0300

    Remove conflicting capacitor config files, keep only JSON

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'AAWSA.Bulkmeter',
-  appName: 'Bulk Meter Reader',
-  webDir: 'build'
-};
-
-export default config;
