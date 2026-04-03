db797ccb08c14713fdecb78101af08c6fec3e8c2
beki65 <bekig65@gmail.com>
2026-04-03 14:17:41 +0300

    Fix localhost in src/config.js to correct IP for mobile app

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 const ENV = {
   development: {
-    API_URL: 'http://localhost:8000/api',
+    API_URL: 'http://192.168.1.216:8000/api',
   },
   production: {
     API_URL: 'https://bulk-meter-mobile.onrender.com/api',
